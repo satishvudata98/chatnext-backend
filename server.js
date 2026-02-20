29b569419ef3c796ea9cc5436778b2ca8729dd23
satishvudata98 <satishvudat98@gmail.com>
2026-02-20 17:39:50 +0530

    explicit port binding fix

--- server.js
+++ server.js
@@ -451,8 +451,6 @@ wss.on("connection", ws => {
 
 });
 
-server.listen(PORT, () => {
-
-    console.log(`Server running on http://localhost:${PORT}`);
-
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
